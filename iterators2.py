0ed3cbeccf57dc4ec342f728f0f6b7efeb1a472c
Athmik A <athmik27@gmail.com>
2026-08-27 11:32:53 +0530

    Implement Square_Number iterator for powers of two
    Added Square_Number class to iterate powers of two.

--- iterators2.py
+++ iterators2.py
@@ -15,4 +15,19 @@ class My_Number:
 my_num=My_Number(5)
 for i in my_num:
     print(i)
-
+#both gives same output but with different range
+class Square_Number:
+    def __init__(self):
+        self.num = 1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.num <= 10:
+            value=2 ** self.num 
+            self.num+=1
+            return value
+        else:
+            raise StopIteration
+sq_num = Square_Number()
+for i in sq_num:
+    print(i)
